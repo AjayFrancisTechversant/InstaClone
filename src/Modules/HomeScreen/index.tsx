cd9fc5e9787269a16c4a7d95db0e5a1d82ee9158
AjayFrancisTechversant <ajay.francis@techversantinfotech.com>
2024-10-09 14:24:51 +0530

    .

--- src/Modules/HomeScreen/index.tsx
+++ src/Modules/HomeScreen/index.tsx
@@ -1,27 +1,29 @@
-import {FlatList} from 'react-native';
-import React from 'react';
+import React, {useMemo} from 'react';
+import {Animated, FlatList, ListRenderItem} from 'react-native';
 import {useScreenContext} from '../../Contexts/ScreenContext';
 import StoriesComponent from '../../Components/StoriesComponent';
 import PostsContainerComponent from '../../Components/PostsContainerComponent';
 import styles from './style';
 
-const HomeScreen = () => {
+const HomeScreen: React.FC = () => {
   const screenContext = useScreenContext();
-  const screenStyles = styles(
+  const screenStyles = useMemo(() => styles(
     screenContext.height,
     screenContext.width,
     screenContext.isTypeTablet,
     screenContext,
-  );
+  ), [screenContext]);
+
+  const renderItem: ListRenderItem<never> = () => null;
+
   return (
     <FlatList
-    style={screenStyles.container}
-    ListHeaderComponent={<StoriesComponent/>}
-    ListFooterComponent={<PostsContainerComponent/>}
+      style={screenStyles.container}
+      ListHeaderComponent={StoriesComponent}
+      ListFooterComponent={PostsContainerComponent}
       data={[]}
-      renderItem={() => null}
+      renderItem={renderItem}
     />
-     
   );
 };
 
